2961050ae65a0ef36450df761c37e0da4270de54
Michel Carroll <carrmi.0@gmail.com>
2012-05-06 16:19:53 -0400

    Added collisino detection

--- src/game.js
+++ src/game.js
@@ -9,6 +9,7 @@ bullets = [];
 var BALL_SPEED = 2;
 var BULLET_SPEED = 10;
 var BALL_SPAWN_SPEED = 2000;
+var COLLISION_RADIUS = 18;
 
 loadGame = function() {
     var canvas = document.getElementById('main');
@@ -30,6 +31,8 @@ registerShooter = function()
     shooter = new BitmapAnimation(getShooterSheet());
     shooter.x = 5;
     shooter.y = bounds.height / 2;
+    shooter.regX = 0;
+    shooter.regY = 25;
     shooter.gotoAndPlay("idle");
 
     stage.addChild(shooter);
@@ -75,7 +78,7 @@ tick = function()
         var animation = balls[x];
         animation.x -= BALL_SPEED;
 
-        if(animation.x + 50 < bounds.x)
+        if(animation.x < bounds.x)
         {
             balls.splice(x, 1);
             stage.removeChild(animation);
@@ -84,22 +87,37 @@ tick = function()
 
     for(x in bullets)
     {
-        var animation = bullets[x];
-        animation.x += BULLET_SPEED;
+        var bullAnim = bullets[x];
+        bullAnim.x += BULLET_SPEED;
 
-        if(animation.x > bounds.width)
+        if(bullAnim.x > bounds.width)
         {
             bullets.splice(x, 1);
-            stage.removeChild(animation);
+            stage.removeChild(bullAnim);
+        }
+
+        //COLLISION TEST
+        for(y in balls)
+        {
+            var collisionBall = balls[y];
+            if(getDistanceBetween(bullAnim.x, bullAnim.y, collisionBall.x, collisionBall.y) < COLLISION_RADIUS)
+            {
+                bullets.splice(x, 1);
+                balls.splice(y, 1);
+                stage.removeChild(collisionBall);
+                stage.removeChild(bullAnim);
+            }
         }
     }
+
     stage.update();
 };
+
 
 launchBullet = function()
 {
     var originX = 10;
-    var originY = shooter.y + 20;
+    var originY = shooter.y;
 
     var bulletAnimation = getBulletBitmap();
     bulletAnimation.x = originX;
@@ -124,7 +142,7 @@ moveCanvas = function(/**MouseEvent*/ e)
 {
     if(shooter)
     {
-        shooter.y = e.stageY - 25;
+        shooter.y = e.stageY;
     }
 };
 
@@ -164,8 +182,11 @@ getBulletBitmap = function()
     var bulletImg = new Image();
     bulletImg.src = "assets/images/bullet.png";
 
-    return new Bitmap(bulletImg);
-}
+    var bitmap = new Bitmap(bulletImg);
+    bitmap.regX = 5;
+    bitmap.regY = 5;
+    return bitmap;
+};
 
 
 spawnBlueBallAt = function(/**int*/ y)
@@ -174,6 +195,8 @@ spawnBlueBallAt = function(/**int*/ y)
     var ballAnimation = new BitmapAnimation(getBallSheet());
     ballAnimation.x = bounds.width;
     ballAnimation.y = y;
+    ballAnimation.regX = 15;
+    ballAnimation.regY = 15;
     ballAnimation.gotoAndPlay("blink");
     balls[ballId] = ballAnimation;
     stage.addChild(ballAnimation);
@@ -184,4 +207,12 @@ startSpawningBalls = function()
     setInterval(function() {
         spawnBlueBallAt(Math.random() * bounds.height);
     }, BALL_SPAWN_SPEED);
+};
+
+getDistanceBetween = function(x1, y1, x2, y2)
+{
+    var deltaX = Math.abs(x2 - x1);
+    var deltaY = Math.abs(y2 - y1);
+
+    return Math.sqrt( Math.pow(deltaX, 2) + Math.pow(deltaY, 2) );
 }
